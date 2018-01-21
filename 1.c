5dc990fccbfdc1b23002013e8f6fe118c7a4354c
Pavikeerthi <35652616+Pavikeerthi@users.noreply.github.com>
2018-01-21 15:20:28 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<cconio.h)
+void main()
+{
+int num;
+printf("enter any number");
+scanf("%d",&num);
+if(num>0)
+{
+printf("number is positive");
+}
+if(num<o)
+{
+printf("number is negative");
+}
+if(num==0)
+{
+printf("number is zero");
+}
+getch();
+}
